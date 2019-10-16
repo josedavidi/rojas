1242f15665962bf84a01acbe3f0d2d72a53dbe73
David Isama <ispwebdavid@gmail.com>
2019-10-16 17:55:21 -0500

    update inform

--- views/detalleInforme.php
+++ views/detalleInforme.php
@@ -10,13 +10,41 @@
 
 	$tareas = $db->query("SELECT procesos.nombre as proceso_nombre FROM equipo_tareas INNER JOIN procesos ON equipo_tareas.proceso_id=procesos.id  WHERE eti ='$eti' ") or die('error'.mysqli_errno($db));
 
+	$galeria = $db->query("SELECT * FROM equipo_fotos WHERE eti='$eti'")or die('error'.mysqli_errno($db));
 	
 
 ?>
 
 
 <div class="card">
-  <img src="<?php  //echo str_replace('../', '', $verInforme['imagen'])  ?>" class="card-img-top">
+  <!-- inicia galeria -->
+  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/equipos/avatar.jpg" class="d-block w-100" alt="...">
+    </div>
+    <?php
+    	while($fila = mysqli_fetch_array($galeria))
+    	{
+    		echo '
+    			<div class="carousel-item">
+			      <img src="'.str_replace('../', '', $fila["nombre"]).'" class="d-block w-100" alt="...">
+			    </div>
+    		';
+    	}
+    ?>
+    
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+  <!-- termina galeria -->
   <div class="card-body">
     <h5 class="card-title">Observacion</h5>
     <input type="hidden" name="eti" value="<?php echo $eti ?>">
